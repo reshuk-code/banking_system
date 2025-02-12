66923492d3bf51616880d135cd507a897842ed06
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-02-12 20:16:56 +0545

    Update server.js

--- forWeb/server.js
+++ forWeb/server.js
@@ -1,3 +1,17 @@
+/*
+ * This code belongs to reshuk-code.
+ * Contact: business.reshuksapkota@gmail.com
+ * Git-Hub: https://github.com/reshuk-code
+ * 
+ * This program is open source and built for a personal project.
+ * You are free to use, modify, and distribute this code for non-commercial purposes.
+ * However, commercial use of this code is strictly prohibited.
+ * 
+ * © 2025 Reshuk Sapkota. All rights reserved.
+ */
+
+
+
 import express from 'express';
 const app = express()
 app.get('/' , (req , res)=>{
